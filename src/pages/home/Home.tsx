78350c804766adedc8a148986e071fc6c4c6acb4
万亚飞 <wanyafei91@163.com>
2022-01-18 10:34:39 +0800

    build: package依赖变更

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-import 'react-region-picker/lib/style.css';
 import style from '../../assets/styles/home.scss';
 
 export default function Home() {
